707934cba7a300a747052fb24803a3e4d399326f
Niklas Kaaf <niklas.kaaf@stud.hn.de>
2024-12-23 20:44:25 +0100

    Optimize state structure

--- minimax.py
+++ minimax.py
@@ -1,16 +1,16 @@
 import logging
 import sys
 from dataclasses import dataclass, field
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 import graphviz
+import numpy
 
 # ----- User parameters ----- #
 
 # Rules: https://www.mastersofgames.com/rules/royal-ur-rules.htm
 # Rules from Tom Scott vs. Finkel
-NUM_OF_PIECES_PER_PLAYER = 5
-STEPS_IN_FUTURE = 2
+STEPS_IN_FUTURE = 5
 PLAYER_1_MIN = True
 ROSETTE_9_IS_SAFE = True
 
@@ -23,15 +23,21 @@ EVAL_MULTIPLIER_ATTACKER = -1.5
 EVAL_ADDER_KILL_HAPPENS = 100
 
 # Visualization
-VISUALIZE = True
+VISUALIZE = False
 VIZ_THROWS = [2, 3]
 
 # ----- Constants ----- #
 
-PLACE_ROSETTE = 3
-PLACE_ROSETTE_SAFE = 6 if ROSETTE_9_IS_SAFE else PLACE_ROSETTE
-PLACE_START = -1
-PLACE_FINISH = -2
+PLACE_FINISH = numpy.uint8(0)
+PLACE_START = numpy.uint8(1)
+NUM_OF_PIECES_PER_PLAYER = 5
+MASK_PIECE_0 = numpy.uint32(0xF)
+MASK_PIECE_1 = numpy.uint32(MASK_PIECE_0 << 4)
+MASK_PIECE_2 = numpy.uint32(MASK_PIECE_1 << 4)
+MASK_PIECE_3 = numpy.uint32(MASK_PIECE_2 << 4)
+MASK_PIECE_4 = numpy.uint32(MASK_PIECE_3 << 4)
+ROSETTE_SAFE = 0x9
+ROSETTES = [5, 15]
 
 
 def _get_file_handler(filename: str) -> logging.FileHandler:
@@ -58,13 +64,29 @@ class ListIndexSafe(list):
             return -1
 
 
+def get_piece_mask(piece_index: int) -> numpy.uint32:
+    if piece_index == 0:
+        return MASK_PIECE_0
+
+    if piece_index == 1:
+        return MASK_PIECE_1
+
+    if piece_index == 2:
+        return MASK_PIECE_2
+
+    if piece_index == 3:
+        return MASK_PIECE_3
+
+    if piece_index == 4:
+        return MASK_PIECE_4
+
+
 @dataclass
 class State:
-    game_board: List[int]
     score_1: int
     score_2: int
-    pieces_1: List[int]
-    pieces_2: List[int]
+    pieces_1: numpy.uint32
+    pieces_2: numpy.uint32
     current_player: int
     other_player: int
     dice: int = field(init=False, default=-1)
@@ -77,7 +99,6 @@ class State:
     eval: float = field(init=False, default=0)
 
     def copy(self) -> "State":
-        game_board = self.game_board.copy()
         score_1 = self.score_1
         score_2 = self.score_2
         pieces_1 = self.pieces_1.copy()
@@ -85,7 +106,7 @@ class State:
         current_player = self.current_player
         other_player = self.other_player
 
-        state = State(game_board, score_1, score_2, pieces_1, pieces_2, current_player,
+        state = State(score_1, score_2, pieces_1, pieces_2, current_player,
                       other_player)
 
         return state
@@ -94,7 +115,6 @@ class State:
         return (f"\tPosition: {self.pos}\n"
                 f"\tCurrent player: {self.current_player} - other_player: {self.other_player}\n"
                 f"\tIs Second Throw: {self.second_throw}\n"
-                f"\tBoard state: {self.game_board}\n"
                 f"\tPieces 1: {self.pieces_1} - Pieces 2: {self.pieces_2}\n"
                 f"\tScore 1: {self.score_1} - Score 2: {self.score_2}\n")
 
@@ -111,18 +131,27 @@ class State:
         self.current_player = self.other_player
         self.other_player = tmp
 
-    def piece_move(self, player: int, piece_index: int, from_index: int, to_index: int) -> None:
-        if from_index != PLACE_START:
-            self.game_board[from_index] -= player
-
-        if to_index not in [PLACE_START, PLACE_FINISH]:
-            self.game_board[to_index] += player
-
+    def piece_move(self, player: int, piece_index: int, to_index: numpy.uint8) -> None:
         # set new piece place
-        pieces_pbl = player_based_list(self.pieces_1, self.pieces_2)
-        pieces_pbl[player][piece_index] = to_index
-        self.pieces_1 = pieces_pbl[1]
-        self.pieces_2 = pieces_pbl[2]
+        piece_mask_clear = ~get_piece_mask(piece_index)
+
+        finished = to_index == PLACE_FINISH
+
+        if player == 1:
+            self.pieces_1 &= piece_mask_clear
+            self.pieces_1 |= numpy.uint32(to_index) << (4 * piece_index)
+
+            if finished:
+                self.score_1 += 1
+
+        elif player == 2:
+            self.pieces_2 &= piece_mask_clear
+            self.pieces_2 |= numpy.uint32(to_index) << (4 * piece_index)
+
+            if finished:
+                self.score_2 += 1
+        else:
+            raise Exception(f"Invalid player {player}")
 
 
 def player_based_list(e1, e2) -> list:
@@ -158,6 +187,69 @@ class StateList:
     def get(self, index: int) -> State:
         return self.states[index]
 
+    def __len__(self):
+        return len(self.states)
+
+
+def piece_cannot_finish(current_pos: int, dice: int) -> bool:
+    return current_pos + dice > 16
+
+
+def piece_can_finish(current_pos: int, dice: int) -> bool:
+    return current_pos + dice == 16
+
+
+def any_piece_on_field(pieces: numpy.uint32, pos: int) -> Tuple[bool, Optional[int]]:
+    ret_flag = False
+    piece_index = None
+
+    if pieces & MASK_PIECE_0 == pos:
+        piece_index = 0
+        ret_flag = True
+    elif (pieces & MASK_PIECE_1) >> (4 * 1) == pos:
+        piece_index = 1
+        ret_flag = True
+    elif (pieces & MASK_PIECE_2) >> (4 * 2) == pos:
+        piece_index = 2
+        ret_flag = True
+    elif (pieces & MASK_PIECE_3) >> (4 * 3) == pos:
+        piece_index = 3
+        ret_flag = True
+    elif (pieces & MASK_PIECE_4) >> (4 * 4) == pos:
+        piece_index = 4
+        ret_flag = True
+
+    return ret_flag, piece_index
+
+
+def piece_cannot_move_current_player_is_on_field(current_state: State, current_pos: int) -> bool:
+    pieces_current_player = player_based_list(current_state.pieces_1, current_state.pieces_2)[
+        current_state.current_player]
+
+    next_pos = current_pos + current_state.dice
+
+    return any_piece_on_field(pieces_current_player, next_pos)[0]
+
+
+def piece_cannot_move_other_player_is_on_safe(current_state: State, current_pos: int) -> bool:
+    if current_pos + current_state.dice != ROSETTE_SAFE:
+        # Player will not move to Rosette Safe Space
+        return False
+
+    return other_player_is_on_field(current_state, current_pos)[0]
+
+
+def other_player_is_on_field(current_state: State, current_pos: int) -> Tuple[bool, Optional[int]]:
+    next_pos = current_pos + current_state.dice
+
+    if not (6 <= next_pos <= 13):
+        # War zone is only between 6 and 13
+        return False, None
+
+    pieces_other = player_based_list(current_state.pieces_1, current_state.pieces_2)[current_state.other_player]
+
+    return any_piece_on_field(pieces_other, next_pos)
+
 
 class MinimaxSimulation:
     # evaluation
@@ -169,81 +261,22 @@ class MinimaxSimulation:
     kill_distances_multiplier = [None, 1 / 4, 3 / 8, 1 / 4, 1 / 16]
 
     def __init__(self) -> None:
-        # States:
-        # 0 -> No player on this field
-        # 1 -> Player1 on this field
-        # 2 -> Player2 on this field
-        # 3 -> Rosette (another throw)
-        # 4 = 3 + 1 -> Player 1 on rosette
-        # 5 = 3 + 2 -> Player 2 on rosette
-        self.game_board = [PLACE_ROSETTE, 0, 0, 0, PLACE_ROSETTE, 0, 0, 0, 0, PLACE_ROSETTE_SAFE, 1, 0, 0, 0,
-                           PLACE_ROSETTE, 0, 0, 0, PLACE_ROSETTE, 0]
-        # self.game_board = [PLACE_ROSETTE, 0, 0, 0, PLACE_ROSETTE, 0, 1, 0, 2, PLACE_ROSETTE_SAFE, 1, 0, 0, 0,
-        #                   PLACE_ROSETTE, 0, 0, 0, PLACE_ROSETTE, 0]
-
-        # Indices of game_board path for both players
-        self.path_1 = ListIndexSafe([3, 2, 1, 0, 6, 7, 8, 9, 10, 11, 12, 13, 5, 4])
-        self.path_2 = ListIndexSafe([17, 16, 15, 14, 6, 7, 8, 9, 10, 11, 12, 13, 19, 18])
-        self.paths: List[ListIndexSafe] = player_based_list(self.path_1, self.path_2)
-
         self.state_list = StateList()
 
         # ----- Start State ----- #
 
         # Indices of game_board places for both players
-        # -1 -> Start
-        # -2 -> Finish
-        pieces_1 = [PLACE_START] * NUM_OF_PIECES_PER_PLAYER
-        # pieces_1[0] = 6
-        # pieces_1[1] = 10
-
-        pieces_2 = [PLACE_START] * NUM_OF_PIECES_PER_PLAYER
-        # pieces_2[1] = 8
+        pieces_1 = numpy.uint32(PLACE_START) | numpy.uint32(PLACE_START) << (4 * 1) | numpy.uint32(PLACE_START) << (
+                4 * 2) | numpy.uint32(PLACE_START) << (4 * 3) | numpy.uint32(PLACE_START) << (4 * 4)
+        pieces_2 = numpy.uint32(PLACE_START) | numpy.uint32(PLACE_START) << (4 * 1) | numpy.uint32(PLACE_START) << (
+                4 * 2) | numpy.uint32(PLACE_START) << (4 * 3) | numpy.uint32(PLACE_START) << (4 * 4)
 
         # Number of pieces in finish for both players
         score_1 = 0
         score_2 = 0
-        # Start state
-        game_board = self.game_board
 
         self.start_state = self.state_list.add_new_state(
-            State(game_board, score_1, score_2, pieces_1, pieces_2, 1, 2))
-
-    def piece_cannot_finish(self, current_player: int, next_path_index: int) -> bool:
-        path = self.paths[current_player]
-        return next_path_index > len(path)
-
-    def piece_can_finish(self, current_player: int, next_path_index: int) -> bool:
-        path = self.paths[current_player]
-        return next_path_index == len(path)
-
-    def piece_cannot_move_current_player_is_on_field(self, current_state: State,
-                                                     next_path_index: int) -> bool:
-        next_place_index = self.paths[current_state.current_player][next_path_index]
-
-        places_current_player = player_based_list(current_state.pieces_1, current_state.pieces_2)[
-            current_state.current_player]
-
-        return any([place == next_place_index for place in places_current_player])
-
-    def get_next_path_index(self, current_player: int, place_index: int, dice: int) -> int:
-        path = self.paths[current_player]
-        return path.index_safe(place_index) + dice
-
-    def piece_cannot_move_other_player_is_on_safe(self, current_state: State,
-                                                  next_path_index: int) -> bool:
-        path = self.paths[current_state.current_player]
-
-        return current_state.game_board[
-            path[next_path_index]] - current_state.other_player == PLACE_ROSETTE_SAFE
-
-    def other_player_is_on_field(self, current_state: State, next_path_index: int) -> bool:
-        next_place_index = self.paths[current_state.current_player][next_path_index]
-
-        places_other_player = player_based_list(current_state.pieces_1, current_state.pieces_2)[
-            current_state.other_player]
-
-        return any([place == next_place_index for place in places_other_player])
+            State(score_1, score_2, pieces_1, pieces_2, 1, 2))
 
     def evaluation(self, state_source: State, state_new: State) -> float:
         # Simulation will swap the player if no second throw
@@ -316,7 +349,8 @@ class MinimaxSimulation:
 
         return points_total
 
-    def simulate_step(self, current_state: State, piece_index: int, dice: int) -> Optional[State]:
+    @staticmethod
+    def simulate_step(current_state: State, piece_index: int, dice: int) -> Optional[State]:
         # current_state is a copy of the current state and can therefore be modified
 
         current_state.dice = dice
@@ -328,13 +362,10 @@ class MinimaxSimulation:
         current_player = current_state.current_player
         other_player = current_state.other_player
 
-        scores = player_based_list(current_state.score_1, current_state.score_2)
         pieces_current_player = player_based_list(current_state.pieces_1, current_state.pieces_2)[
             current_player]
 
-        place_current_piece = pieces_current_player[piece_index]
-
-        next_path_index = self.get_next_path_index(current_player, place_current_piece, dice)
+        place_current_piece = (pieces_current_player & get_piece_mask(piece_index)) >> (4 *piece_index)
 
         if dice == 0:
             # No movement
@@ -347,29 +378,28 @@ class MinimaxSimulation:
 
             return None
 
-        elif self.piece_cannot_finish(current_player, next_path_index):
+        elif piece_cannot_finish(place_current_piece, dice):
             # Move cannot be done, because the piece has to be finished perfectly
             # -> This is not a valid move
 
             return None
 
-        elif self.piece_can_finish(current_player, next_path_index):
+        elif piece_can_finish(place_current_piece, dice):
             # Piece is in finish with next move
 
             # Piece moves to finish
-            current_state.piece_move(current_player, piece_index, place_current_piece, PLACE_FINISH)
-            scores[current_player] += 1
+            current_state.piece_move(current_player, piece_index, PLACE_FINISH)
 
             state_new = current_state
 
-        elif self.piece_cannot_move_current_player_is_on_field(current_state, next_path_index):
+        elif piece_cannot_move_current_player_is_on_field(current_state, place_current_piece):
             # Move cannot be done, on the field is already a piece of the current_player,
             # -> This is not a valid move
 
             return None
 
-        elif ROSETTE_9_IS_SAFE and self.piece_cannot_move_other_player_is_on_safe(current_state,
-                                                                                  next_path_index):
+        elif ROSETTE_9_IS_SAFE and piece_cannot_move_other_player_is_on_safe(current_state,
+                                                                             place_current_piece):
             # Move cannot be done, because this rosette is a safe spot for the other player
             # -> This is not a valid move
 
@@ -377,23 +407,17 @@ class MinimaxSimulation:
 
         else:
             # Valid move and not in finish
-            next_place_index = self.paths[current_state.current_player][next_path_index]
 
-            if self.other_player_is_on_field(current_state, next_path_index):
+            flag, piece_index_other_player = other_player_is_on_field(current_state, place_current_piece)
+            if flag:
                 # Other player will be caught and returned to start
+                current_state.piece_move(other_player, piece_index_other_player, PLACE_START)
 
-                piece_index_other_player = \
-                    player_based_list(current_state.pieces_1, current_state.pieces_2)[
-                        other_player].index(next_place_index)
-                current_state.piece_move(other_player, piece_index_other_player, next_place_index,
-                                         PLACE_START)
-
-            if current_state.game_board[next_place_index] == PLACE_ROSETTE:
+            if place_current_piece in ROSETTES:
                 current_state.second_throw = True
 
             # current player moves from current place to new place
-            current_state.piece_move(current_player, piece_index, place_current_piece,
-                                     next_place_index)
+            current_state.piece_move(current_player, piece_index, place_current_piece + dice)
 
             state_new = current_state
 
@@ -414,8 +438,8 @@ class MinimaxSimulation:
                 current_player = state.other_player
 
             color = "green" if current_player == 1 else "red"
-            graph.node(str(state.pos), f"{state.eval}\n{state.dice}\n{state.moved_piece}", _attributes={"color": color})
-
+            graph.node(str(state.pos), f"ID: {state.pos}\nS: {state.eval}\nD: {state.dice}\nMP: {state.moved_piece}",
+                       _attributes={"color": color})
         for state in self.state_list:
             for child in state.children:
                 graph.edge(str(state.pos), str(child))
@@ -492,9 +516,9 @@ class MinimaxSimulation:
                         state_new.check_win(current_state.current_player)
 
                         # ----- Evaluation ----- #
-                        score = self.evaluation(current_state, state_new)
-                        logger_eval.info(f"{step},{score}")
-                        state_new.eval = score
+                        # score = self.evaluation(current_state, state_new)
+                        # logger_eval.info(f"{step},{score}")
+                        # state_new.eval = score
 
                         logger_out.info(f"Simulated state: \n{state_new}")
 
@@ -542,7 +566,12 @@ class MinimaxSimulation:
             self.visualize()
             self.visualize_path()
 
+        print(len(self.state_list))
+
 
 if __name__ == "__main__":
+    import datetime
+    print(datetime.datetime.now())
     simulation = MinimaxSimulation()
     simulation.start()
+    print(datetime.datetime.now())
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import unittest
 
-from minimax import MinimaxSimulation, State, NUM_OF_PIECES_PER_PLAYER, PLACE_START, PLACE_FINISH
+import numpy
+
+from minimax import MinimaxSimulation, State, PLACE_START, PLACE_FINISH, get_piece_mask
 
 
 class MinimaxTest(unittest.TestCase):
@@ -8,15 +10,13 @@ class MinimaxTest(unittest.TestCase):
     def setUp(self) -> None:
         self.sim = MinimaxSimulation()
 
-        self.game_board_default = self.sim.game_board.copy()
         self.score_default = 0
-        self.pieces_default = [PLACE_START] * NUM_OF_PIECES_PER_PLAYER
+        self.pieces_default = numpy.uint32(PLACE_START)
 
         self.current_player_default = 1
         self.other_player_default = 2
 
-        self.state_default = State(self.game_board_default.copy(), self.score_default,
-                                   self.score_default, self.pieces_default.copy(),
+        self.state_default = State(self.score_default, self.score_default, self.pieces_default.copy(),
                                    self.pieces_default.copy(), self.current_player_default, self.other_player_default)
 
     def test0(self) -> None:
@@ -28,6 +28,8 @@ class MinimaxTest(unittest.TestCase):
 
         expected_state = self.state_default.copy()
         expected_state.swap_player()
+        expected_state.dice = dice
+        expected_state.moved_piece = piece_index
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
 
@@ -39,7 +41,9 @@ class MinimaxTest(unittest.TestCase):
         dice = 1
 
         current_state = self.state_default.copy()
-        current_state.pieces_1[piece_index] = PLACE_FINISH
+        piece_mask_clear = ~get_piece_mask(piece_index)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(PLACE_FINISH) << (4 * piece_index)
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
 
@@ -51,8 +55,9 @@ class MinimaxTest(unittest.TestCase):
         dice = 3
 
         current_state = self.state_default.copy()
-        current_state.game_board[5] += current_state.current_player
-        current_state.pieces_1[piece_index] = 5
+        piece_mask_clear = ~get_piece_mask(piece_index)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(14) << (4 * piece_index)
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
 
@@ -64,11 +69,16 @@ class MinimaxTest(unittest.TestCase):
         dice = 2
 
         current_state = self.state_default.copy()
-        current_state.game_board[5] += current_state.current_player
-        current_state.pieces_1[piece_index] = 5
+        piece_mask_clear = ~get_piece_mask(piece_index)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(14) << (4 * piece_index)
 
         expected_state = self.state_default.copy()
-        expected_state.pieces_1[piece_index] = PLACE_FINISH
+        expected_state.score_1 = 1
+        expected_state.pieces_1 &= piece_mask_clear
+        expected_state.pieces_1 |= numpy.uint32(PLACE_FINISH) << (4 * piece_index)
+        expected_state.dice = dice
+        expected_state.moved_piece = piece_index
         expected_state.swap_player()
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
@@ -81,8 +91,9 @@ class MinimaxTest(unittest.TestCase):
         dice = 1
 
         current_state = self.state_default.copy()
-        current_state.game_board[3] += current_state.current_player
-        current_state.pieces_1[1] = 3
+        piece_mask_clear = ~get_piece_mask(piece_index + 1)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(2) << (4 * (piece_index + 1))
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
 
@@ -94,10 +105,11 @@ class MinimaxTest(unittest.TestCase):
         dice = 1
 
         current_state = self.state_default.copy()
-        current_state.game_board[9] += current_state.other_player
-        current_state.pieces_2[0] = 9
-        current_state.game_board[8] += current_state.current_player
-        current_state.pieces_1[piece_index] = 8
+        piece_mask_clear = ~get_piece_mask(piece_index)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(8) << (4 * piece_index)
+        current_state.pieces_2 &= piece_mask_clear
+        current_state.pieces_2 |= numpy.uint32(9) << (4 * piece_index)
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
 
@@ -109,12 +121,15 @@ class MinimaxTest(unittest.TestCase):
         dice = 1
 
         current_state = self.state_default.copy()
-        current_state.game_board[6] += current_state.current_player
-        current_state.pieces_1[piece_index] = 6
+        piece_mask_clear = ~get_piece_mask(piece_index)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(6) << (4 * piece_index)
 
         expected_state = self.state_default.copy()
-        expected_state.game_board[7] += current_state.current_player
-        expected_state.pieces_1[piece_index] = 7
+        expected_state.pieces_1 &= piece_mask_clear
+        expected_state.pieces_1 |= numpy.uint32(7) << (4 * piece_index)
+        expected_state.dice = dice
+        expected_state.moved_piece = piece_index
         expected_state.swap_player()
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
@@ -127,15 +142,19 @@ class MinimaxTest(unittest.TestCase):
         dice = 1
 
         current_state = self.state_default.copy()
-        current_state.game_board[7] += current_state.current_player
-        current_state.pieces_1[piece_index] = 7
-        current_state.game_board[8] += current_state.other_player
-        current_state.pieces_2[0] = 8
+        piece_mask_clear = ~get_piece_mask(piece_index)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(7) << (4 * piece_index)
+        current_state.pieces_2 &= piece_mask_clear
+        current_state.pieces_2 |= numpy.uint32(8) << (4 * piece_index)
 
         expected_state = self.state_default.copy()
-        expected_state.game_board[8] += current_state.current_player
-        expected_state.pieces_1[piece_index] = 8
-        expected_state.pieces_2[0] = PLACE_START
+        expected_state.pieces_1 &= piece_mask_clear
+        expected_state.pieces_1 |= numpy.uint32(8) << (4 * piece_index)
+        expected_state.pieces_2 &= piece_mask_clear
+        expected_state.pieces_2 |= numpy.uint32(PLACE_START) << (4 * piece_index)
+        expected_state.dice = dice
+        expected_state.moved_piece = piece_index
         expected_state.swap_player()
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
@@ -148,14 +167,38 @@ class MinimaxTest(unittest.TestCase):
         dice = 1
 
         current_state = self.state_default.copy()
-        current_state.game_board[1] += current_state.current_player
-        current_state.pieces_1[piece_index] = 1
+        piece_mask_clear = ~get_piece_mask(piece_index)
+        current_state.pieces_1 &= piece_mask_clear
+        current_state.pieces_1 |= numpy.uint32(5) << (4 * piece_index)
 
         expected_state = self.state_default.copy()
-        expected_state.game_board[0] += current_state.current_player
-        expected_state.pieces_1[piece_index] = 0
+        expected_state.pieces_1 &= piece_mask_clear
+        expected_state.pieces_1 |= numpy.uint32(6) << (4 * piece_index)
+        expected_state.dice = dice
+        expected_state.moved_piece = piece_index
         expected_state.second_throw = True
 
         state_new = self.sim.simulate_step(current_state, piece_index, dice)
 
         self.assertEqual(expected_state, state_new)
+
+
+class ArithmeticTest(unittest.TestCase):
+
+    def test0(self) -> None:
+        state = State(0, 0, numpy.uint32(0xA4FDA), numpy.uint32(0xD), 1, 2)
+
+        state.piece_move(1, 0, numpy.uint8(0x9))
+        self.assertEqual(numpy.uint32(0xA4FD9), state.pieces_1)
+
+        state.piece_move(1, 1, numpy.uint8(0x9))
+        self.assertEqual(numpy.uint32(0xA4F99), state.pieces_1)
+
+        state.piece_move(1, 2, numpy.uint8(0x9))
+        self.assertEqual(numpy.uint32(0xA4999), state.pieces_1)
+
+        state.piece_move(1, 3, numpy.uint8(0x9))
+        self.assertEqual(numpy.uint32(0xA9999), state.pieces_1)
+
+        state.piece_move(1, 4, numpy.uint8(0x9))
+        self.assertEqual(numpy.uint32(0x99999), state.pieces_1)
